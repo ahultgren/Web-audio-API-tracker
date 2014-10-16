062a35cf7880dacd4a6d4e8f0f7f8c93b238e01a
Andreas Hultgren <me@andreashultgren.se>
2014-10-16 12:15:32 +0200

    I got a coin!

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
-"use strict";
+'use strict';
 
-console.log("what now?...");
+var Wad = require('wad/src/wad');
+
+var coin = new Wad({
+    source  : 'square',
+    volume  : 0.5,
+    pitch   : 828,
+    detune  : 0,
+    env     : {
+        attack  : 0.0,
+        decay   : 0.2384,
+        sustain : 0.4918,
+        hold    : 0.0005912,
+        release : 0
+    }
+});
+
+function playCoin () {
+  console.log(coin.play());
+  coin.play({
+    detune: 500,
+    wait: 0.094
+  });
+
+  setTimeout(coin.stop.bind(coin), 420);
+  setTimeout(coin.stop.bind(coin), 420);
+}
+
+playCoin();
